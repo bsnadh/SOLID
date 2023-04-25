49f7aa080460771d94c9fbbf7f7dd1dc8fe3cfe4
Bishnu Adhikari <bsnadh984@gmail.com>
2023-04-25 15:13:08 +0545

    Implementation of interface segregation

--- src/solid/Cube.java
+++ src/solid/Cube.java
@@ -1,8 +1,16 @@
 package solid;
 
-public class Cube implements Shape{
+public class Cube implements Shape,Shape3D{
     @Override
     public double area() {
         return 100;
+    }
+
+    // Now, we're leaving the classes which cannot calculate volume untouched
+    // We've implemented new shape3d interface for 3D shapes
+    // We've also divided large interface into smaller ones.
+    @Override
+    public double volume() {
+        return 0;
     }
 }
--- src/solid/Rectangle.java
+++ src/solid/Rectangle.java
@@ -5,11 +5,4 @@ public class Rectangle implements Shape{
     public double area() {
         return 20;
     }
-
-    // Interfaces should not force classes to implement what they
-    //    can't do.
-    @Override
-    public double volume() {
-        return 0;
-    }
 }
