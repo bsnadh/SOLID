aa13ca6919d458da560b62cd22e389f75a49dc4e
Bishnu Adhikari <bsnadh984@gmail.com>
2023-04-25 15:06:28 +0545

    violation of interface segregation

--- src/solid/Circle.java
+++ src/solid/Circle.java
@@ -8,4 +8,13 @@ public class Circle implements Shape {
     public double area() {
         return Math.PI * Math.pow(getRadius(),2);
     }
+
+    // Let's say circle cannot calculate volume
+    // We're telling circle to do what it can not do
+    // Interfaces should not force classes to implement what they
+    //    can't do.
+    @Override
+    public double volume() {
+        return 0;
+    }
 }
--- src/solid/Rectangle.java
+++ src/solid/Rectangle.java
@@ -5,4 +5,11 @@ public class Rectangle implements Shape{
     public double area() {
         return 20;
     }
+
+    // Interfaces should not force classes to implement what they
+    //    can't do.
+    @Override
+    public double volume() {
+        return 0;
+    }
 }
